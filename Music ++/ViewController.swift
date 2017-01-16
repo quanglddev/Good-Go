06e563bde02d35dfe448c5e9ba534e1ea7ad247e
QUANG <crzQag@luongs-macbook-air.local>
2017-01-16 09:44:30 +0700

    Initial Commit

---
+++ Music ++/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Music ++
+//
+//  Created by QUANG on 1/16/17.
+//  Copyright © 2017 Q.U.A.N.G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
